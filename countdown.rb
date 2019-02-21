bd1a6871625e0bb87c2e875b6ef0d25eb375b962
Dennis Chung <chungdennis6@gmail.com>
2019-02-21 22:59:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,16 @@
 #write your code here
 
 def countdown
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-return "HAPPY NEW YEAR!"
+def countdown_with_sleep(int)
+  sleep int 
 end
 
-def countdown_with_sleep(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    sleep(1)
-    seconds -= 1
-  end
-return "HAPPY NEW YEAR!"
+ def countdown(x)
+  x = x
+  while x > 0 
+ puts "#{x} SECOND(S)!"
+  x -= 1
+end
+
+ return "HAPPY NEW YEAR!"
 end 
